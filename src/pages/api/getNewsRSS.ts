61cceec222ad8b845d846c6b9a03df97531b612f
Emika <khaw.rkk@gmail.com>
2026-03-15 13:51:39 +0700

    fix build failure using Claude

--- src/pages/api/getNewsRSS.ts
+++ src/pages/api/getNewsRSS.ts
@@ -30,3 +30,9 @@
 //     );
 //   }
 // }
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(404).end();
+}
